08372a0936583257d3a5c68159f4647d014183aa
Takagi Kensuke <janus.wel.3@gmail.com>
2023-07-01 23:26:16 +0900

    :heavy_plus_sign::wrench::sparkles: apollo server with examples

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `
+type Book {
+  title: String
+  author: String
+}
+
+type Query {
+  books: [Book]
+}
+`;
+
+const books = [
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    author: "J.K. Rowling",
+  },
+  {
+    title: "Jurassic Park",
+    author: "Michael Crichton",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: {
+    port: 4000,
+  },
+});
+
+console.log(`🚀 Server ready at ${url}`);
