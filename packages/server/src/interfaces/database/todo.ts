7492001781afa9e0753915bb3d1ac6fe1f0a0906
Takagi Kensuke <janus.wel.3@gmail.com>
2023-07-17 13:36:11 +0900

    :recycle: refactor

--- packages/server/src/interfaces/database/todo.ts
+++ packages/server/src/interfaces/database/todo.ts
@@ -1,4 +1,3 @@
-import { PrismaClient } from "@prisma/client";
 import { Entity } from "../../entities/todo.js";
 import { prismaClientFactory } from "./prisma.js";
 
@@ -24,63 +23,64 @@ export interface TodoRepository {
   deleteTodo(args: DeleteTodoArgs): Promise<Entity>;
 }
 
-async function getUncompletedTodos(prisma: PrismaClient) {
-  const todos = await prisma.todo.findMany({
-    where: { isCompleted: false },
-  });
-  return todos;
-}
+export function todoRepositoryFactory(databaseUrl: string): TodoRepository {
+  const prisma = prismaClientFactory(databaseUrl);
 
-async function getTodoById(prisma: PrismaClient, id: string) {
-  const todo = await prisma.todo.findUnique({
-    where: { id },
-  });
-
-  if (!todo) {
-    throw new Error(`Todo with id ${id} is not found`);
+  async function getUncompletedTodos() {
+    const todos = await prisma.todo.findMany({
+      where: { isCompleted: false },
+    });
+    return todos;
   }
 
-  return todo;
-}
+  async function getTodoById(id: string) {
+    const todo = await prisma.todo.findUnique({
+      where: { id },
+    });
 
-async function createTodo(prisma: PrismaClient, args: CreateTodoArgs) {
-  const todo = await prisma.todo.create({
-    data: {
-      title: args.title,
-    },
-  });
-  return todo;
-}
+    if (!todo) {
+      throw new Error(`Todo with id ${id} is not found`);
+    }
 
-async function updateTodo(prisma: PrismaClient, args: UpdateTodoArgs) {
-  getTodoById(prisma, args.id);
+    return todo;
+  }
 
-  const todo = await prisma.todo.update({
-    where: { id: args.id },
-    data: {
-      title: args.title || undefined,
-      isCompleted: args.isCompleted || undefined,
-    },
-  });
-  return todo;
-}
+  async function createTodo(args: CreateTodoArgs) {
+    const todo = await prisma.todo.create({
+      data: {
+        title: args.title,
+      },
+    });
+    return todo;
+  }
 
-async function deleteTodo(prisma: PrismaClient, args: DeleteTodoArgs) {
-  getTodoById(prisma, args.id);
+  async function updateTodo(args: UpdateTodoArgs) {
+    getTodoById(args.id);
 
-  const todo = await prisma.todo.delete({
-    where: { id: args.id },
-  });
-  return todo;
-}
+    const todo = await prisma.todo.update({
+      where: { id: args.id },
+      data: {
+        title: args.title || undefined,
+        isCompleted: args.isCompleted || undefined,
+      },
+    });
+    return todo;
+  }
 
-export function todoRepositoryFactory(databaseUrl: string): TodoRepository {
-  const prismaClient = prismaClientFactory(databaseUrl);
+  async function deleteTodo(args: DeleteTodoArgs) {
+    getTodoById(args.id);
+
+    const todo = await prisma.todo.delete({
+      where: { id: args.id },
+    });
+    return todo;
+  }
+
   return {
-    getUncompletedTodos: () => getUncompletedTodos(prismaClient),
-    getTodoById: (id) => getTodoById(prismaClient, id),
-    createTodo: (args) => createTodo(prismaClient, args),
-    updateTodo: (args) => updateTodo(prismaClient, args),
-    deleteTodo: (args) => deleteTodo(prismaClient, args),
+    getUncompletedTodos,
+    getTodoById,
+    createTodo,
+    updateTodo,
+    deleteTodo,
   };
 }
